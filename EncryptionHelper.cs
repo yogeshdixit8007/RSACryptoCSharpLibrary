7dee9e2e127aa75f8eed7dff40fc81bbc009408c
yogeshdixit8007 <yogeshdxt8007@gmail.com>
2025-04-22 10:46:44 +0530

    Update EncryptionHelper.cs

--- EncryptionHelper.cs
+++ EncryptionHelper.cs
@@ -4,25 +4,24 @@ using System.Text;
 
 public static class EncryptionHelper
 {
-    [UnmanagedCallersOnly(EntryPoint = "Encrypt", CallingConvention = CallingConvention.Cdecl)]
-    public static IntPtr Encrypt(IntPtr inputPtr, IntPtr publicKeyPtr)
-    {
-        string input = Marshal.PtrToStringUTF8(inputPtr)!;
-        string publicKey = Marshal.PtrToStringUTF8(publicKeyPtr)!;
+    [UnmanagedCallersOnly(EntryPoint = "Encrypt")]
+public static IntPtr Encrypt(IntPtr inputPtr, IntPtr publicKeyPtr)
+{
+    string input = Marshal.PtrToStringUTF8(inputPtr)!;
+    string publicKey = Marshal.PtrToStringUTF8(publicKeyPtr)!;
 
-        // TODO: Replace with real encryption using publicKey
-        string result = "EncryptedData: " + input;
-        return Marshal.StringToCoTaskMemUTF8(result);
-    }
+    string result = "EncryptedData: " + input;
+    return Marshal.StringToCoTaskMemUTF8(result);
+}
 
-    [UnmanagedCallersOnly(EntryPoint = "Decrypt", CallingConvention = CallingConvention.Cdecl)]
-    public static IntPtr Decrypt(IntPtr encryptedPtr, IntPtr privateKeyPtr)
-    {
-        string encrypted = Marshal.PtrToStringUTF8(encryptedPtr)!;
-        string privateKey = Marshal.PtrToStringUTF8(privateKeyPtr)!;
+[UnmanagedCallersOnly(EntryPoint = "Decrypt")]
+public static IntPtr Decrypt(IntPtr encryptedPtr, IntPtr privateKeyPtr)
+{
+    string encrypted = Marshal.PtrToStringUTF8(encryptedPtr)!;
+    string privateKey = Marshal.PtrToStringUTF8(privateKeyPtr)!;
 
-        // TODO: Replace with real decryption using privateKey
-        string result = "DecryptedData: " + encrypted;
-        return Marshal.StringToCoTaskMemUTF8(result);
-    }
+    string result = "DecryptedData: " + encrypted;
+    return Marshal.StringToCoTaskMemUTF8(result);
+}
+
 }
